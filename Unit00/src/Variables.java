8e8d5584e7e83ef161ba40f626adb0e2fe1e0abf
fiona-m-mit <99138469+fiona-giraffe@users.noreply.github.com>
2022-08-18 10:29:21 -0700

    yay

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -18,34 +18,46 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = 10000;
+		int intOne = 1000000;
+		long longOne = -999999999;
 
 		//decimal variables
-
+		float floatOne = 10.8f;
+		double doubleOne = 98.88;
 
 		//other integer types
-
+		char charOne = 'A';
 
 		//other types
-
+		boolean booleanOne = true;
+		String StringOne = "ahoy";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
+		System.out.println("*Fiona McSherry       08/18/2022*");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
-
-
-
-
-
-
-
-
-
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          other types          *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*StringOne = "+StringOne+"\t\t*");
+		System.out.println("/////////////////////////////////");
+		
 	}
 }
