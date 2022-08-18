ff87d6fae1a86188e30a20f267f770a7b0c6aeac
fiona-m-mit <99138469+fiona-giraffe@users.noreply.github.com>
2022-08-17 17:21:26 -0700

    work on unit 0

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Fiona McSherry \n\n" );
+		System.out.println("Cat" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("           _/\\___/\\_             " );
+		System.out.println("           |       |             " );
+		System.out.println("           |       |             " );
+		System.out.println("           |_______|             " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Fiona McSherry
+//Date - August 17, 2022
+//Class - APCSA
+//Lab  - Unit 0 AsciiBox
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Fiona McSherry \t  08/17/2022 \n\n" );
+		System.out.println("+++++++++++++++++++++++++++ " );
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+ ----------------------- + ");
+		System.out.println("+++++++++++++++++++++++++++ ");
+
+	}
+}
