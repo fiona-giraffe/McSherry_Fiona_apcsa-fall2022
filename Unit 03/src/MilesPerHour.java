8bd23a2d533afb7f80fb890fdcce5c4c798c4d44
fiona-m-mit <99138469+fiona-giraffe@users.noreply.github.com>
2022-08-22 10:37:14 -0700

    woo

--- Unit 03/src/MilesPerHour.java
+++ Unit 03/src/MilesPerHour.java
@@ -12,7 +12,8 @@ import static java.lang.Math.*;
 
 public class MilesPerHour
 {
-	private int distance, hours, minutes;
+	Scanner keyboard = new Scanner(System.in);
+	private int distance, hours, minutes, newmph;
 	private double mph;
 
 	public MilesPerHour()
@@ -23,24 +24,34 @@ public class MilesPerHour
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
-		double temp = mins/60;
-		hrs += temp;
-		mph = dist/hrs;
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		out.print("Enter the distance :: ");
+		dist = keyboard.nextInt();
 		
+		out.print("Enter the hours :: ");
+		hrs = keyboard.nextInt();
+		
+		out.print("Enter the minutes :: ");
+		mins = keyboard.nextInt();
 	}
 
 	public void calcMPH()
 	{
-		
+		double temp = (double) hours;
+		temp += (double) minutes/60;
+		mph = distance/temp;
+		newmph = (int) Math.round(mph);
 	}
 
 	public void print()
 	{
-		
+		out.printf(distance + " miles in " + hours + " hours and " + minutes + " minutes = " + newmph + " MPH.");
 	}
 	
 	//create a print or toString or both
