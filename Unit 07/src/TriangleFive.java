4a957e748351e90ee0cbdeb4055332e8a0e87167
fiona-m-mit <99138469+fiona-giraffe@users.noreply.github.com>
2022-09-02 11:35:31 -0700

    yayyyyyy

--- Unit 07/src/TriangleFive.java
+++ Unit 07/src/TriangleFive.java
@@ -11,23 +11,55 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		letter = '0';
+		amount = 0;
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter('A');
+		setAmount(0);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
 		String output="";
+		char L = letter;
+		int x = amount;
+		int z = amount;
+		
+		for(int i = amount; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				
+				while(x > 0) {
+					output += "" + L;
+					x = x-1;
+				}
+				z = z - 1;
+				x = z;
+				
+				if(L == 'Z')
+					L = 'A';
+				else
+					L = (char) (L + 1);
+				
+				output += " ";
+			}
+		x = amount;
+		z = amount;
+		L = letter;
+		output += "\n";
+		}
 		return output;
 	}
 }
--- Unit 07/src/Triples.java
+++ Unit 07/src/Triples.java
@@ -11,38 +11,68 @@ public class Triples
 	public Triples()
 	{
 		this(0);
+		number = 0;
 	}
 
 	public Triples(int num)
 	{
-
-
+		setNum(0);
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num;
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
+		int factor;
+		for(int i=1; i <= Math.min(a, Math.min(b, c)); i++) {
+			if(a%i==0 && b%i==0 && c%i==00) {
+				factor = i;
+				if (Math.max(factor, max)==factor) {
+					max = factor;
+				}
+			}
+		}
 
-
-
-		return 1;
+		return max;
+	}
+	
+	private boolean oddOrEven(int a, int b, int c) {
+		if(a%2==0 && b%2!=0 && c%2!=0) {
+			return true;
+		}
+		else if(a%2!=0 && b%2==0 && c%2!=0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private boolean theorem(int a, int b, int c) {
+		if(Math.pow((double) a, 2)+ Math.pow((double) b, 2)==Math.pow((double) c, 2)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+		for(int a=0; a<=number; a++) {
+			for(int b=0; b<=number; b++) {
+				for(int c=0; c<=number; c++) {
+					if(greatestCommonFactor(a, b, c)==1 && oddOrEven(a, b, c) && theorem(a, b, c) && a<b && b<c && a<c) {
+						output = output + "\n" + a + " " + b + " " + c;
+					}
+				}
+			}
+		}
 		return output+"\n";
 	}
 }
